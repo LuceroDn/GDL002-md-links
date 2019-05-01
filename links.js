9d5ffe7394f0b5307b9f36bbfa29415d006e664e
Lucero Dn <lucero_dn@hotmail.es>
2019-05-01 18:23:38 -0500

    Se creo alchivo links.js y se agrego funcion de leer archivo en el mismo

---
+++ links.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+//const path = require ('path');
+  
+// Module to read files 
+module.exports = (pathFile, options) => {
+    return new Promise((resolve, reject)=> {
+      // Leer el archivo
+      fs.readFile(pathFile, function(err, data){
+        if (err){
+          return reject(err);
+        }
+        resolve(data.toString());
+      });
+    });
+}; 
